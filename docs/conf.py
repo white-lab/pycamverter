30303c3a5e5b7d797d4b620f7ebe4fa005859a19
Nader Morshed <morshed.nader@gmail.com>
2017-02-28 12:58:45 -0500

    Fixed one last docs file

--- docs/conf.py
+++ docs/conf.py
@@ -311,7 +311,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pyproteome', u'pyproteome Documentation',
+    (master_doc, 'pycamverter', u'pycamverter Documentation',
      [author], 1)
 ]
 
@@ -325,8 +325,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pyproteome', u'pyproteome Documentation',
-   author, 'pyproteome', 'One line description of project.',
+  (master_doc, 'pycamverter', u'pycamverter Documentation',
+   author, 'pycamverter', 'One line description of project.',
    'Miscellaneous'),
 ]
 
