db5f9b672b86d6d3f7e7f49fa2f3c9853328b015
Nader Morshed <morshed.nader@gmail.com>
2017-03-06 14:07:27 -0500

    Don't indent output (saves file size)

--- pycamv/export.py
+++ pycamv/export.py
@@ -512,9 +512,9 @@ def export_to_camv(
 
     if out_path.endswith(".gz"):
         with gzip.GzipFile(out_path, 'w') as f:
-            f.write(json.dumps(data, indent=2).encode("utf-8"))
+            f.write(json.dumps(data, indent=None).encode("utf-8"))
     else:
         with open(out_path, "w") as f:
-            json.dump(data, f, indent=2)
+            json.dump(data, f, indent=None)
 
     return data
