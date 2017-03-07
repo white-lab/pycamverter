58e016f73fc44e0b5fbdf074f2a38fd4d3eb318a
Nader Morshed <morshed.nader@gmail.com>
2017-03-07 14:19:53 -0500

    Added LenGen util class

--- pycamv/utils.py
+++ pycamv/utils.py
@@ -1,7 +1,11 @@
 """Utility functions used in other modules."""
 
-import math
+from __future__ import absolute_import, division
+
+import copy
 from collections import OrderedDict, Callable
+import itertools
+import math
 
 
 def nCr(n, r):
@@ -9,12 +13,27 @@ def nCr(n, r):
     return f(n) / f(r) / f(n - r)
 
 
+class LenGen(object):
+    def __init__(self, gen, length):
+        self.gen = gen
+        self.length = length
+
+    def __call__(self):
+        return itertools.islice(self.gen(), self.length)
+
+    def __len__(self):
+        return self.length
+
+
 class DefaultOrderedDict(OrderedDict):
     # Source: http://stackoverflow.com/a/6190500/562769
     def __init__(self, default_factory=None, *a, **kw):
-        if (default_factory is not None and
-           not isinstance(default_factory, Callable)):
-            raise TypeError('first argument must be callable')
+        if (
+            default_factory is not None and
+            not isinstance(default_factory, Callable)
+        ):
+            raise TypeError("first argument must be callable")
+
         OrderedDict.__init__(self, *a, **kw)
         self.default_factory = default_factory
 
@@ -27,7 +46,9 @@ class DefaultOrderedDict(OrderedDict):
     def __missing__(self, key):
         if self.default_factory is None:
             raise KeyError(key)
+
         self[key] = value = self.default_factory()
+        
         return value
 
     def __reduce__(self):
@@ -35,18 +56,26 @@ class DefaultOrderedDict(OrderedDict):
             args = tuple()
         else:
             args = self.default_factory,
+
         return type(self), args, None, None, self.items()
 
     def copy(self):
         return self.__copy__()
 
     def __copy__(self):
-        return type(self)(self.default_factory, self)
+        return type(self)(
+            self.default_factory,
+            self,
+        )
 
     def __deepcopy__(self, memo):
-        return type(self)(self.default_factory,
-                          copy.deepcopy(self.items()))
+        return type(self)(
+            self.default_factory,
+            copy.deepcopy(self.items()),
+        )
 
     def __repr__(self):
-        return 'OrderedDefaultDict(%s, %s)' % (self.default_factory,
-                                               OrderedDict.__repr__(self))
+        return "OrderedDefaultDict({}, {})".format(
+            self.default_factory,
+            OrderedDict.__repr__(self),
+        )
