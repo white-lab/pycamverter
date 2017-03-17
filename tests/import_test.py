5d281979d1199ae32b2d4d95fc697b24a2ee6ffe
Nader Morshed <morshed.nader@gmail.com>
2017-03-17 14:55:19 -0400

    Updated test

--- tests/import_test.py
+++ tests/import_test.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 class ImportTest(TestCase):
     def test_imports(self):
         from pycamv import (
-            compare, discoverer, export, fragments, gen_sequences, main,
-            mascot, masses, ms_labels, pep_query, proteowizard,
+            compare, discoverer, export, fragments, gen_sequences, losses,
+            main, mascot, masses, ms_labels, pep_query, proteowizard,
             regexes, scans, search, validate, version,
         )
