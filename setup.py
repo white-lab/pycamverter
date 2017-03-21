311aaa32cbbb471e3ddcf1c2f5d8e0fdc1bafe2e
Nader Morshed <morshed.nader@gmail.com>
2017-03-21 14:02:41 -0400

    Added pypy to list

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Scientific/Engineering",
     ],
     test_suite="tests",
