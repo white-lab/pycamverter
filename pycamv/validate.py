95fa8221f9a4889f24a824bbbe9446a9868cbfed
Nader Morshed <morshed.nader@gmail.com>
2017-04-05 19:11:37 -0400

    Forbit semicolon in raw path

--- pycamv/validate.py
+++ pycamv/validate.py
@@ -214,6 +214,14 @@ def validate_spectra(
                 .format(base_raw, base_raw_paths)
             )
 
+    for path in raw_paths:
+        if ";" in path:
+            raise Exception(
+                "Semicolon character not allowed in raw path: {}".format(
+                    path,
+                )
+            )
+
     # Generate sequences
     LOGGER.info(
         "Generating all possible sequence-modification combinations."
