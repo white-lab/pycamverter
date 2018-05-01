d71e43f406c6a8bf6ca7265ff73f013337e711da
Nader Morshed <morshed.nader@gmail.com>
2018-04-30 21:15:41 -0400

    Fixed pip10

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 import os
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from pip.download import PipSession
 from setuptools import setup, find_packages
 
